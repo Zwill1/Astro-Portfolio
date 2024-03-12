40aebd2cdac04c285f0ebb4ab2f8c0ea40dbbda1
Mark <zwill_1@hotmail.com>
2024-03-11 18:31:54 -0600

    fixing minor UI styles and navigation widths to match layout

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,16 +3,15 @@ import Hamburger from "./Hamburger.jsx";
 import HamburgerClose from "./HamburgerClose.jsx";
 
 function Navigation() {
+  const [nav, setNav] = useState(false);
 
-    const [nav, setNav] = useState(false);
-
-    const handleClick = () => setNav(!nav);
-    const handleClose = () => setNav(!nav);
+  const handleClick = () => setNav(!nav);
+  const handleClose = () => setNav(!nav);
 
   return (
     <>
       <section className="bg-white text-black sticky top-0 z-50 shadow-md">
-        <div className="flex justify-between md:w-full h-14 w-10/12 mx-auto container items-center">
+        <div className="flex justify-between h-14 w-10/12 mx-auto items-center">
           <div>
             <h1 className="flex">
               <a
@@ -25,36 +24,24 @@ function Navigation() {
           </div>
           <div>
             <ul className="hidden lg:flex pr-4">
-                <>
-                  <li className="p-4">
-                    <a href="/">
-                      Home
-                    </a>
-                  </li>
-                  <li className="p-4">
-                    <a href="/portfolio">
-                      Portfolio
-                    </a>
-                  </li>
-                  <li className="p-4">
-                    <a href="/education">
-                      Education
-                    </a>
-                  </li>
-                  <li className="p-4">
-                    <a href="/contact">
-                      Contact
-                    </a>
-                  </li>
-                </>
+              <>
+                <li className="p-4">
+                  <a href="/">Home</a>
+                </li>
+                <li className="p-4">
+                  <a href="/portfolio">Portfolio</a>
+                </li>
+                <li className="p-4">
+                  <a href="/education">Education</a>
+                </li>
+                <li className="p-4">
+                  <a href="/contact">Contact</a>
+                </li>
+              </>
             </ul>
           </div>
           <div className="lg:hidden" onClick={handleClick}>
-            {!nav ? (
-              <Hamburger />
-            ) : (
-              <HamburgerClose />
-            )}
+            {!nav ? <Hamburger /> : <HamburgerClose />}
           </div>
         </div>
         <ul
@@ -62,32 +49,40 @@ function Navigation() {
             !nav ? "hidden" : "absolute bg-white w-full lg:hidden z-50"
           }
         >
-        <>
+          <>
             <li className="w-full mx-auto text-center bg-[#f4f4f4]">
-            <a href="/" class="block pt-4 pb-4" onClick={handleClose}>
+              <a href="/" class="block pt-4 pb-4" onClick={handleClose}>
                 Home
-            </a>
+              </a>
             </li>
             <li className="w-full mx-auto text-center">
-            <a href="/portfolio" class="block pt-4 pb-4" onClick={handleClose}>
+              <a
+                href="/portfolio"
+                class="block pt-4 pb-4"
+                onClick={handleClose}
+              >
                 Portfolio
-            </a>
+              </a>
             </li>
             <li className="w-full mx-auto text-center bg-[#f4f4f4]">
-            <a href="/education" class="block pt-4 pb-4" onClick={handleClose}>
+              <a
+                href="/education"
+                class="block pt-4 pb-4"
+                onClick={handleClose}
+              >
                 Education
-            </a>
+              </a>
             </li>
             <li className="w-full mx-auto text-center">
-            <a href="/contact" class="block pt-4 pb-4" onClick={handleClose}>
+              <a href="/contact" class="block pt-4 pb-4" onClick={handleClose}>
                 Contact
-            </a>
+              </a>
             </li>
-        </>
+          </>
         </ul>
       </section>
     </>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
